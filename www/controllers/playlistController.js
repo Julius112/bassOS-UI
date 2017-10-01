2cfcda5ea79e9fc33a62034661d2bd8def8bd15c
Julius Niedworok <julius.n@gmx.net>
2017-10-01 21:41:58 +0000

    Added automatic playlist updates

--- www/controllers/playlistController.js
+++ www/controllers/playlistController.js
@@ -1,10 +1,43 @@
 angular.module('bassOS').controller('playlistCtl', function($scope, mpd) {
-	$scope.curSong = "Loading";
-	mpd.mpd_client.on('Connect', function(){
-		$scope.$apply(function() {
-			//console.log( $scope.curSong);
-			$scope.currentSong = mpd.mpd_client.getCurrentSongID();
-			$scope.playlist = mpd.mpd_client.getQueue().getSongs();
+	/* INIT */
+	$scope.playing = false;
+
+	/* MPD CONNECT */
+	mpd.mpd_client.on('Connect', () => {
+		$scope.$apply(() => {
+			var state = mpd.mpd_client.getState();
+			$scope.currentSong = state.current_song.id;
+			if (state.playstate === "PLAYING")
+				$scope.playing = true;
+			else
+				$scope.playing = false;
+			$scope.queue = state.current_queue.getSongs();
 		});
 	});
+
+	/* QUEUE UPDATE */
+	mpd.mpd_client.on('QueueChanged', (newQueue) => {
+		console.log("change");
+		$scope.$apply(() => {
+			$scope.queue = newQueue.getSongs();
+		});
+	});
+
+	/* STATE UPDATE */
+	mpd.mpd_client.on('StateChanged', (newState) => {
+		console.log("change");
+		$scope.$apply(() => {
+			$scope.currentSong = newState.current_song.id;
+			if (newState.playstate === "PLAYING")
+				$scope.playing = true;
+			else
+				$scope.playing = false;
+			$scope.queue = newQueue.getSongs();
+		});
+	});
+
+
+	$scope.playSong = (songId) => {
+		mpd.mpd_client.playById(songId);
+	}
 }); 
