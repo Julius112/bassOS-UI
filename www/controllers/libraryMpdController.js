fb8161571bdcb7eb913507fc3d1a1dc09d79780d
Julius Niedworok <julius.n@gmx.net>
2018-01-11 22:14:07 +0000

    Added alert which asks how to queue song

--- www/controllers/libraryMpdController.js
+++ www/controllers/libraryMpdController.js
@@ -2,7 +2,17 @@ angular.module('bassOS').controller('libraryMpdCtl', function($scope, mpd) {
 	$scope.searchlist = [];
 
 	$scope.select_song = (song) => {
-		ons.notification.prompt({message: song.getDisplayName()})
+		ons.notification.alert(
+			{
+				message: song.getDisplayName(),
+				buttonLabels: ["Play Last", "Play Next", "Cancel"],
+				title: "Queue Song",
+				cancelable: true,
+				primaryButtonIndex: 0
+			}
+		).then((buttonId) => {
+			console.log(buttonId)
+		});
 	};
 
 	$scope.search_change = () => {
