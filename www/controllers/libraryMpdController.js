e643a91573416e865e2d4420b2e6480d3b67325d
Julius Niedworok <julius.n@gmx.net>
2018-01-12 19:53:38 +0000

    Added Song queueing capabilities in playlist and mediathek

--- www/controllers/libraryMpdController.js
+++ www/controllers/libraryMpdController.js
@@ -1,17 +1,41 @@
 angular.module('bassOS').controller('libraryMpdCtl', function($scope, mpd) {
 	$scope.searchlist = [];
 
+	/*** Feature dropped due to race condition when song is not queued but the old last song is moved to the next song
+	var queue_add_next = (song) => {
+		ons.notification.toast({
+			message: "Eingereiht: " + song.getDisplayName(),
+			timeout: 1500
+		});
+		mpd.mpd_client.getQueue().addSongByFile(song.getPath());
+		var queue_songs = mpd.mpd_client.getQueue().getSongs();
+		mpd.mpd_client.moveSongOnQueueById(queue_songs[queue_songs.length - 1].getId(), mpd.mpd_client.getNextSongQueueIndex());
+	};
+	*/
+
+	var queue_add_end = (song) => {
+		ons.notification.toast({
+			message: "Eingereiht: " + song.getDisplayName(),
+			timeout: 1500
+		});
+		mpd.mpd_client.getQueue().addSongByFile(song.getPath());
+	};
+
 	$scope.select_song = (song) => {
 		ons.notification.alert(
 			{
 				message: song.getDisplayName(),
-				buttonLabels: ["Play Last", "Play Next", "Cancel"],
-				title: "Queue Song",
+				//buttonLabels: ["Nächstes", "Am Ende", "Abbrechen"],
+				buttonLabels: ["Ok", "Abbrechen"],
+				title: "Song einreihen",
 				cancelable: true,
 				primaryButtonIndex: 0
 			}
 		).then((buttonId) => {
-			console.log(buttonId)
+			if(buttonId == 0)
+				queue_add_next(song);
+			else if (buttonId == 1)
+				queue_add_end(song);
 		});
 	};
 
--- www/controllers/playlistController.js
+++ www/controllers/playlistController.js
@@ -2,6 +2,18 @@ angular.module('bassOS').controller('playlistCtl', function($scope, mpd) {
 	/* INIT */
 	$scope.playing = false;
 
+	var playSongNext = (song) => {
+		var current_pos = mpd.mpd_client.getCurrentSongQueueIndex();
+		if (song.getQueuePosition() < current_pos)
+			mpd.mpd_client.moveSongOnQueueById(song.getId(), current_pos);
+		else
+			mpd.mpd_client.moveSongOnQueueById(song.getId(), current_pos + 1);
+	};
+
+	var playSong = (song) => {
+		mpd.mpd_client.playById(song.getId());
+	};
+
 	/* MPD CONNECT */
 	mpd.mpd_client.on('Connect', () => {
 		$scope.$apply(() => {
@@ -36,8 +48,20 @@ angular.module('bassOS').controller('playlistCtl', function($scope, mpd) {
 		});
 	});
 
-
-	$scope.playSong = (songId) => {
-		mpd.mpd_client.playById(songId);
-	}
+	$scope.clickSong = (song) => {
+		ons.notification.alert(
+			{
+				message: song.getDisplayName(),
+				buttonLabels: ["Als Nächstes", "Jetzt", "Abbrechen"],
+				title: "Song Abspielen",
+				cancelable: true,
+				primaryButtonIndex: 0
+			}
+		).then((buttonId) => {
+			if(buttonId == 0)
+				playSongNext(song);
+			else if (buttonId == 1)
+				playSong(song);
+		});
+	};
 }); 
